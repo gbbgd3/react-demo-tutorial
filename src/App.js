57e624f14d7b021af9638d62ee10bd88ebb92324
gbbgd3 <williambrar6@gmail.com>
2023-01-12 13:33:29 -0600

    Added Blog Section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import './App.css';
 import './Navbar';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
 
   return (
     <div className="App">
       <Navbar />
 
       <div className="content">
-        <Home></Home>
+        <Home />
       </div>
     </div>
   );
